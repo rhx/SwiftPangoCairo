4ee19f3e93398d8e7336e1704401c23c55ebdedd
Rene Hexel <r.hexel@griffith.edu.au>
2020-08-30 09:23:34 +1000

    Use local repository

--- Package.swift
+++ Package.swift
@@ -8,8 +8,10 @@ let package = Package(
         .library(name: "PangoCairo", targets: ["PangoCairo"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftCairo.git", .branch("master")),
-        .package(url: "https://github.com/rhx/SwiftPango.git", .branch("master"))
+        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftCairo", .branch("master")),
+        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftPango", .branch("master"))
+        //.package(url: "https://github.com/rhx/SwiftCairo.git", .branch("master")),
+        //.package(url: "https://github.com/rhx/SwiftPango.git", .branch("master"))
     ],
     targets: [
         .target(name: "PangoCairo", dependencies: ["Cairo", "Pango"]),
