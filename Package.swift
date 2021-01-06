774dc6ef9710a016ccc75fa38734851197160938
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-06 10:07:16 +1000

    s/master/main/

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
     name: "PangoCairo",
     products: [ .library(name: "PangoCairo", targets: ["PangoCairo"]) ],
     dependencies: [
-        .package(name: "Cairo", url: "https://github.com/rhx/SwiftCairo.git", .branch("master")),
-        .package(name: "Pango", url: "https://github.com/rhx/SwiftPango.git", .branch("master"))
+        .package(name: "Cairo", url: "https://github.com/rhx/SwiftCairo.git", .branch("main")),
+        .package(name: "Pango", url: "https://github.com/rhx/SwiftPango.git", .branch("main"))
     ],
     targets: [
         .target(name: "PangoCairo", dependencies: ["Cairo", "Pango"]),
