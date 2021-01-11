250762135a24da2fe19a9d513a447080f1fe86f7
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-11 16:18:24 +1000

    Split CI scripts and add badges

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
     name: "PangoCairo",
     products: [ .library(name: "PangoCairo", targets: ["PangoCairo"]) ],
     dependencies: [
-        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("development")),
-        .package(name: "Cairo", url: "https://github.com/rhx/SwiftCairo.git", .branch("development")),
-        .package(name: "Pango", url: "https://github.com/rhx/SwiftPango.git", .branch("development"))
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
+        .package(name: "Cairo", url: "https://github.com/rhx/SwiftCairo.git", .branch("main")),
+        .package(name: "Pango", url: "https://github.com/rhx/SwiftPango.git", .branch("main"))
     ],
     targets: [
         .target(name: "PangoCairo", dependencies: ["Cairo", "Pango"]),
