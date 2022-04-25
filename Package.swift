0058afb4be14441c9ad08d2b0ac2cef7c6a7180b
Rene Hexel <rhx@users.noreply.github.com>
2022-04-26 05:37:43 +1000

    Add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/rhx/gir2swift.git",  branch: "development"),
         .package(url: "https://github.com/rhx/SwiftCairo.git", branch: "development"),
-        .package(url: "https://github.com/rhx/SwiftPango.git", branch: "development")
+        .package(url: "https://github.com/rhx/SwiftPango.git", branch: "development"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .systemLibrary(
